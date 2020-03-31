af07e6893a36baa8f63675909b5abdf59acff695
Martin Helmich <kontakt@martin-helmich.de>
2020-03-31 10:01:36 +0200

    Add examples files for 10th edition

---
+++ examples/13/listing_13-04.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['snowboard'] = 'EXT:snowboard/Configuration/Rte/Custom.yaml';
---
+++ examples/13/listing_13-11.php
@@ -0,0 +1,13 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+   'snowboard',
+   'Configuration/TSconfig/Page/page_access.typoscript',
+   'Snowboardschule: Zugriffsrechte'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+   'snowboard',
+   'Configuration/TSconfig/Page/fields.typoscript',
+   'Snowboardschule: Backend-Anpassungen'
+);
