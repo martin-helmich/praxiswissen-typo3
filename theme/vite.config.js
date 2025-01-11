6430df24d3a714a1b63595f47487658b75a3f308
Martin Helmich <kontakt@martin-helmich.de>
2025-01-11 12:13:18 +0100

    Adjust build job to new template

--- theme/vite.config.js
+++ theme/vite.config.js
@@ -3,6 +3,9 @@ import * as path from "path";
 
 export default {
     root: path.resolve(__dirname, 'src'),
+    build: {
+        outDir: path.resolve(__dirname, 'dist'),
+    },
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
